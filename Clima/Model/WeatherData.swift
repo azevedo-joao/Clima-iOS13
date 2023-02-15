c3088791427269d652c25c880b401508a21c2b4c
João Azevedo <40271272+azevedo-joao@users.noreply.github.com>
2023-02-15 19:48:13 +0100

    WeatherData added

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by João Azevedo on 27.12.22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let id: Int
+}
